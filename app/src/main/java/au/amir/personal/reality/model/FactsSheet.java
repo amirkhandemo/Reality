176484a8e291ade46ea86da577df9487205d03f6
aakhans@gmail.com <aakhans@gmail.com>
2015-03-22 12:15:10 +1100

    first commit

---
+++ app/src/main/java/au/amir/personal/reality/model/FactsSheet.java
@@ -0,0 +1,64 @@
+package au.amir.personal.reality.model;
+
+import java.util.List;
+
+/**
+ * Created by amir on 3/20/2015.
+ */
+
+public class FactsSheet {
+    private String title;
+    private List<Row> rows;
+
+    public FactsSheet() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<Row> rows) {
+        this.rows = rows;
+    }
+
+    public class Row{
+        private String title;
+        private String description;
+        private String imageHref;
+
+        public Row() {
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getImageHref() {
+            return imageHref;
+        }
+
+        public void setImageHref(String imageHref) {
+            this.imageHref = imageHref;
+        }
+    }
+}
